10190c41016852fcd39d35e9fd77fb333cf86f5c
Kyle Spaans <kyle@spaans.ca>
2023-05-05 13:50:05 -0400

    add power level to record

--- record.js
+++ record.js
@@ -3,11 +3,16 @@ import readline from 'node:readline'
 const rl = readline.createInterface({ input: process.stdin })
 
 const start_time = new Date()
+let power = 100
 
 rl.on('line', l => {
   const n = new Date()
+  const [temp, new_power] = l.split(' ')
+  if (new_power !== undefined) {
+    power = new_power
+  }
   const roast_secs = Math.floor((n.getTime() - start_time.getTime()) / 1000)
-  console.log(roast_secs, l)
+  console.log(roast_secs, temp, power)
   console.error(`${Math.floor(roast_secs / 60)}:${roast_secs % 60}`)
 })
 
